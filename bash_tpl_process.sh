9279f78da7d7f45005429a4649e348bc32cc3f1c
Ricky Cook <mail@thatpanda.com>
2014-06-16 22:17:55 +1000

    Fix whitespace with var sub in tables

--- bash_tpl_process.sh
+++ bash_tpl_process.sh
@@ -6,6 +6,9 @@ while IFS= read line ; do
     while [[ "$line" =~ (\$\{[a-zA-Z_][a-zA-Z_0-9]*\}) ]] ; do
         LHS=${BASH_REMATCH[1]}
         RHS="$(eval echo "\"$LHS\"")"
+        whitespace=$((${#RHS} - ${#LHS}))
+        whitespace="$(printf ' %.0s' $(seq 1 $whitespace))"
+        LHS="$LHS$whitespace"
         line=${line//$LHS/$RHS}
     done
     echo "$line"

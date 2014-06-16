9853da12afee3162a73e6f43d91991c2618772ca
Ricky Cook <mail@thatpanda.com>
2014-06-16 21:11:12 +1000

    Fix whitespace for tpl processor

--- bash_tpl_process.sh
+++ bash_tpl_process.sh
@@ -2,11 +2,11 @@
 source ./private_env_vars
 
 # http://stackoverflow.com/a/2916159/509043
-while read line ; do
+while IFS= read line ; do
     while [[ "$line" =~ (\$\{[a-zA-Z_][a-zA-Z_0-9]*\}) ]] ; do
         LHS=${BASH_REMATCH[1]}
         RHS="$(eval echo "\"$LHS\"")"
         line=${line//$LHS/$RHS}
     done
-    echo $line
+    echo "$line"
 done
